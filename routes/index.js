63a908ea021a0ab95284e272e43dd9b775d8957b
Adarshmnair1999 <a4aadarsh1999@gmail.com>
2022-09-30 17:42:23 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {page:'Home', menuId:'home'});
+});
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', {page:'About Us', menuId:'about'});
+});
+/* GET project page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', {page:'Projects', menuId:'projects'});
+});
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', {page:'Services', menuId:'services'});
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {page:'Contact Us', menuId:'contact'});
+});
+
+
+module.exports = router;
